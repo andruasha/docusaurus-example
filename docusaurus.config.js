d68c1a328e518cb676b8113667adea6482082a73
andruasha <128077686+andruasha@users.noreply.github.com>
2024-01-24 21:06:08 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://mekstack-test.website.yandexcloud.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/index.html',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
