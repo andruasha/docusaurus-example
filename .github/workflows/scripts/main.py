a3719ac8db1e2045106dec6412bf1924044f2467
andruasha <128077686+andruasha@users.noreply.github.com>
2024-01-25 22:31:27 +0300

    fix: changing content-type

--- .github/workflows/scripts/main.py
+++ .github/workflows/scripts/main.py
@@ -1,7 +1,5 @@
 import os
-import re
 import boto3
-
 
 SOURCE_ROOT = os.environ.get("SOURCE-ROOT")
 STORAGE_ACCESS_KEY_ID = os.environ.get("STORAGE-ACCESS-KEY-ID")
@@ -23,9 +21,13 @@ def upload_files_to_s3(path, bucket_name):
         for file in files:
             local_file_path = os.path.join(root, file)
             s3_key = os.path.relpath(local_file_path, path)
-            s3.upload_file(local_file_path, bucket_name, s3_key)
-
-
+            
+            if s3_key == 'assets/css/styles.1c022e41.css':
+                extra_args = {'ContentType': 'text/css'}
+            else:
+                extra_args = {}
+            
+            s3.upload_file(local_file_path, bucket_name, s3_key, ExtraArgs=extra_args)
 
 if __name__ == "__main__":
     upload_files_to_s3(SOURCE_ROOT, STORAGE_BUCKET)
